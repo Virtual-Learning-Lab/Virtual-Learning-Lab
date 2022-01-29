41135a04c9b797aae8b8250392abec431d73d8b7
KevinGiesberts <kevin.giesberts@gmail.com>
2022-01-29 18:42:38 +0100

    Update sensor scripts

--- Virtual Learning Lab program/Assets/CameraRotation.cs
+++ Virtual Learning Lab program/Assets/CameraRotation.cs
@@ -1,61 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
-    private static CameraRotation instance;
-    public static CameraRotation Instance
+    [Header("Tweaks")]
+    [SerializeField] private Quaternion baseRotation = new Quaternion(0, 0, 1, 0);
+
+    private void Start()
     {
-        get
-        {
-            if (instance == null)
-            {
-                instance = FindObjectOfType<CameraRotation>();
-                if (instance == null)
-                {
-                    instance = new GameObject("Spawned CameraRotation", typeof(CameraRotation)).GetComponent<CameraRotation>();
-                }
-            }
-            return instance;
-        }
-        set
-        {
-            instance = value;
-        }
+        GyroManager.Instance.EnableGyro();
     }
 
-    [Header("Logic")]
-    private Gyroscope gyro;
-    private Quaternion rotation;
-    private bool gyroActive;
-
-    public void EnableGyro()
-    {
-        if (gyroActive)
-        {
-            return;
-        }
-
-        if (SystemInfo.supportsGyroscope)
-        {
-            gyro = Input.gyro;
-            gyro.enabled = true;
-        }
-
-        gyroActive = gyro.enabled;
-    }
     private void Update()
     {
-        if (gyroActive)
-        {
-            rotation = gyro.attitude;
-            Debug.Log(rotation);
-        }
+        transform.localRotation = GyroManager.Instance.GetGyroRotation() * baseRotation;
     }
-    public Quaternion GetGyroRotation()
-    {
-        return rotation;
-    }
-
 }
