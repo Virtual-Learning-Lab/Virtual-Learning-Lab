1552783397bdfc7e6ecd410c560eacba1ded6c42
KevinGiesberts <kevin.giesberts@gmail.com>
2022-02-11 09:56:35 +0100

    ConnectToServer.cs Update

--- Virtual Learning Lab program/Assets/Scripts/ConnectToServer.cs
+++ Virtual Learning Lab program/Assets/Scripts/ConnectToServer.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.SceneManagement;
 
-public class ConnectToServer : MonoBehaviour
+public class ConnectToServer : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         PhotonNetwork.ConnectUsingSettings();  
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("Game Scene");
     }
 }
