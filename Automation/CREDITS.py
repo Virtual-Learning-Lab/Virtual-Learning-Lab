aaccbb5330d763abc191ac8e0d970790a9421c43
KevinGiesberts <kevin.giesberts@gmail.com>
2022-02-12 19:43:37 +0100

    Update CREDITS.py

---
+++ Automation/CREDITS.py
@@ -0,0 +1,11 @@
+import tkinter
+from tkinter import *
+
+class main:
+    def __init__(self, window):
+        pass
+
+if __name__ == "__main__":
+    window = Tk()
+    main(window)
+    window.mainloop()
