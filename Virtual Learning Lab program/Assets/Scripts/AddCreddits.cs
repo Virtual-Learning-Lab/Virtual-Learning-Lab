fbb3de99552140cda88cbce947d6edfde6b37c7f
KevinGiesberts <kevin.giesberts@gmail.com>
2022-02-12 10:12:22 +0100

    Starting To read from CREDITS.txt

--- Virtual Learning Lab program/Assets/Scripts/AddCreddits.cs
+++ Virtual Learning Lab program/Assets/Scripts/AddCreddits.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
 
 public class AddCreddits : MonoBehaviour
 {
-    
+    public Transform contentWindow;
+    public GameObject recallTextObject;
+
     void Start()
     {
-        
+        string readFromFilePath = "/Scenes/" + "/Recourses/" + "CREDITS" + ".txt";
     }
 }
