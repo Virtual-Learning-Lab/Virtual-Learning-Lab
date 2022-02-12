3424352d5d81618548d31c1692e412795e007113
KevinGiesberts <kevin.giesberts@gmail.com>
2022-02-12 19:50:30 +0100

    Update CREDITS.py

--- Automation/CREDITS.py
+++ Automation/CREDITS.py
@@ -11,10 +11,21 @@ class main:
         self.title_inp = Entry(self.window)
         self.title_inp.grid(row=0, column=1)
 
-        self.name_lbl = Label(self.window, text="Name: ")
+        self.name_lbl = Label(self.window, text='Name: ')
         self.name_lbl.grid(row=1, column=0)
         self.name_inp = Entry(self.window)
         self.name_inp.grid(row=1, column=1)
+
+        self.link_lbl = Label(self.window, text='Link: ')
+        self.link_lbl.grid(row=2, column=0)
+        self.link_inp = Entry(self.window)
+        self.link_inp.grid(row=2, column=1)
+
+        self.remove_btn = Button(self.window, text='Remove CREDITS: ')
+        self.remove_btn.grid(row=3, column=1)
+
+    def update(self):
+        pass
 
 if __name__ == "__main__":
     window = Tk()
