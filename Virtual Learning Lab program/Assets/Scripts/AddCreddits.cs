535a2c0fb1e545f52e8300dab140773ed9ff0ad0
KevinGiesberts <kevin.giesberts@gmail.com>
2022-02-15 17:45:32 +0100

    Removed all html components

--- Virtual Learning Lab program/Assets/Scripts/AddCreddits.cs
+++ Virtual Learning Lab program/Assets/Scripts/AddCreddits.cs
@@ -18,11 +18,24 @@ public class AddCreddits : MonoBehaviour
     void Start()
     {
         //Get all CREDITS information from CREDTIS.html which is a page on the official Virtual Learning lab website
+        /*<!DOCTYPE html>
+        <html>
+        <head>
+        <title>Credits</title>
+        </head>
+        <body>
+            <h1>Credits</h1>
+            <h2>Founder</h2>
+            <h3>Stephen van Erkelens</h3>
+        </body>*/
+
         var url = "https://virtual-learninglab.github.io/CREDITS.html";
         
         var httpClient = new HttpClient();
         var html = httpClient.GetStringAsync(url);
-        string html_results = html.Result.Replace("<!DOCTYPE html>", "").Replace("<html>", "").Replace("</html>", "");
+
+        //Removing all the html code
+        string html_results = html.Result.Replace("<!DOCTYPE html>", "").Replace("<html>", "").Replace("</html>", "").Replace("Credits", "").Replace("<title>", "").Replace("</title>", "").Replace("<head>", "").Replace("</head>", "").Replace("<h1>", "").Replace("</h1>", "").Replace("<h2>", "").Replace("</h2>", "").Replace("<h3>", "").Replace("</h3>", "").Replace("<body>", "").Replace("</body>", "").Replace("!p!", "\n");
 
         Debug.Log(html_results);
     }
