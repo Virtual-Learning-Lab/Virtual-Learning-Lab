8e5fa2954d788a6999e24b93ee09b8d3c9811c5b
KevinGiesberts <kevin.giesberts@gmail.com>
2022-02-18 14:50:46 +0100

    Update

--- Virtual Learning Lab program/Assets/Scripts/SpawnPlayers.cs
+++ Virtual Learning Lab program/Assets/Scripts/SpawnPlayers.cs
@@ -5,16 +5,27 @@ using Photon.Pun;
 
 public class SpawnPlayers : MonoBehaviour
 {
-    public GameObject playerPrefab;
+    public GameObject playerPrefab_VR;
+    public GameObject playerPrefab_nonVR;
 
     public float minX;
     public float maxX;
     public float minY;
     public float maxY;
+    public string settings_file;
 
     void Start()
     {
+        bool VR = false;
+
         Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
-        PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
+        if (VR == true)
+        {
+            PhotonNetwork.Instantiate(playerPrefab_VR.name, randomPosition, Quaternion.identity);
+        }
+        else
+        {
+            PhotonNetwork.Instantiate(playerPrefab_nonVR.name, randomPosition, Quaternion.identity);
+        }
     }
 }
